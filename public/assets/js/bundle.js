36ea5e287fa3537177c8fb94e2748476a83945b2
AntonyOnScript <santhiagoafk@gmail.com>
2021-07-16 10:16:49 -0300

    Added script wpack on package.json

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Car = function Car(name) {
+  _classCallCheck(this, Car);
+
+  this.name = name;
+};
+
+var car = new Car('Ford');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
